c0ecfc1d4b4e7846f07e6073c5b0388481758e37
Evan <podnov@gmail.com>
2015-10-12 07:22:05 -0500

    cleanup

--- integration-tests-war/src/main/java/com/evanzeimet/queryinfo/it/companies/CompanyQueryInfoBean.java
+++ integration-tests-war/src/main/java/com/evanzeimet/queryinfo/it/companies/CompanyQueryInfoBean.java
@@ -1,6 +1,5 @@
 package com.evanzeimet.queryinfo.it.companies;
 
-import javax.annotation.PostConstruct;
 
 /*
  * #%L
@@ -13,9 +12,9 @@ import javax.annotation.PostConstruct;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +24,7 @@ import javax.annotation.PostConstruct;
  */
 
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
--- integration-tests-war/src/main/java/com/evanzeimet/queryinfo/it/people/PeopleQueryInfoBeanContext.java
+++ integration-tests-war/src/main/java/com/evanzeimet/queryinfo/it/people/PeopleQueryInfoBeanContext.java
@@ -1,6 +1,5 @@
 package com.evanzeimet.queryinfo.it.people;
 
-import com.evanzeimet.queryinfo.jpa.bean.context.QueryInfoBeanContext;
 
 /*
  * #%L
@@ -13,9 +12,9 @@ import com.evanzeimet.queryinfo.jpa.bean.context.QueryInfoBeanContext;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +24,7 @@ import com.evanzeimet.queryinfo.jpa.bean.context.QueryInfoBeanContext;
  */
 
 
+import com.evanzeimet.queryinfo.jpa.bean.context.QueryInfoBeanContext;
 
 public interface PeopleQueryInfoBeanContext
 		extends QueryInfoBeanContext<PersonEntity, PersonEntity, PersonEntity> {
